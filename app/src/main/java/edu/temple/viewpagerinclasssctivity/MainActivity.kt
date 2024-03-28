016e6c9ee4461cb0756a947c6ae96df913a1bc49
Jonathan Augustin <tun45218@temple.edu>
2024-03-28 12:18:00 -0400

    ViewPager2 can now have additional pages.

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -3,7 +3,6 @@ package edu.temple.viewpagerinclasssctivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 
@@ -17,17 +16,22 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.viewPager)
     }
 
+    var numFragments = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         viewPager.adapter = object: FragmentStateAdapter(this) {
-            override fun getItemCount() = 10
+            override fun getItemCount() = numFragments
 
             override fun createFragment(position: Int) = TextFragment.newInstance((position + 1).toString())
-
         }
 
+        newButton.setOnClickListener() {
+            numFragments++
+            (viewPager.adapter as FragmentStateAdapter).notifyDataSetChanged()
+            viewPager.setCurrentItem(numFragments - 1, true)
+        }
     }
-
 }
